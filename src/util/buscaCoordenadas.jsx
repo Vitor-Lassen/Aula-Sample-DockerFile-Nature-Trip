536d1c2f2830eefb803edcc8c3a7db8231e11556
karinaperes <karinaperesg@gmail.com>
2024-08-15 11:57:22 -0300

    Melhorias cadastro de locais, busca cep e busca coordenadas

---
+++ src/util/buscaCoordenadas.jsx
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+async function buscaCoordenadas(coordenadas, setValue) {
+
+    try {
+        const [lat, lon] = coordenadas.split(',')
+
+        if (!lat || !lon) {
+            throw new Error('Coordenadas inválidas.');
+        }
+
+        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`)
+        
+        if (response.data && response.data.address) {
+            const cidade = response.data.address.city || response.data.address.town || response.data.address.village;
+            const estado = response.data.address.state            
+
+            if (cidade && estado) {
+                setValue('cidade', cidade)
+                setValue('estado', estado)            
+
+            } else {
+                throw new Error('Coordenadas não encontradas.')
+            }
+        } else {
+            throw new Error('Resposta da API não contém os dados esperados.')
+        }
+
+    } catch (error) {
+        console.error('Erro ao consultar coordenadas.')        
+    }
+}
+
+export default buscaCoordenadas
